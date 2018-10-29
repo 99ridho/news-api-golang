92a6e7dd0baa8b3310208639b08c1b16a7edac91
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2018-10-29 16:14:35 +0700

    change contract to able to return pagination also

--- domain/news/usecase.go
+++ domain/news/usecase.go
@@ -8,10 +8,10 @@ import (
 
 // NewsUseCase is representing a news business logic
 type NewsUseCase interface {
-	FetchNews(ctx context.Context, limit, cursor int64) ([]*models.News, error)
+	FetchNews(ctx context.Context, limit, cursor int64) ([]*models.News, *models.Pagination, error)
 	InsertNews(ctx context.Context, news *models.News) (*models.News, error)
 	UpdateNews(ctx context.Context, news *models.News) (*models.News, error)
 	DeleteNews(ctx context.Context, news *models.News) (int64, error)
-	FilterNewsByStatus(ctx context.Context, status *models.NewsStatus) ([]*models.News, error)
-	FilterNewsByTopicIDs(ctx context.Context, topicIDs []int64) ([]*models.News, error)
+	FilterNewsByStatus(ctx context.Context, status *models.NewsStatus) ([]*models.News, *models.Pagination, error)
+	FilterNewsByTopicIDs(ctx context.Context, topicIDs []int64) ([]*models.News, *models.Pagination, error)
 }
